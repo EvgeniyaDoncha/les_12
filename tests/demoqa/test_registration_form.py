ef5b2332f6fb1b389cc03ede86beeb48b203a3c9
EvgeniyaDoncha <donchulesko@list.ru>
2025-04-21 15:16:23 +0300

    add selenoid

--- tests/demoqa/test_registration_form.py
+++ tests/demoqa/test_registration_form.py
@@ -1,18 +1,35 @@
+from xml.dom.xmlbuilder import Options
+
 import allure
 from selene import have, by
-
 from dotenv import load_dotenv
-
-
 from selene.support.shared import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
+# Загрузка переменных окружения
+load_dotenv()
+
+# Инициализация браузера (если это необходимо)
+# browser.config.driver = webdriver.Chrome()  # Убедитесь, что драйвер установлен
 
 @allure.title("Successful fill form")
 def test_successful():
-
-
-
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "128.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
     first_name = "Alex"
     last_name = "Egorov"
 
@@ -28,14 +45,9 @@ def test_successful():
         browser.element("#userEmail").set_value("alex@egorov.com")
         browser.element("#genterWrapper").element(by.text("Other")).click()
         browser.element("#userNumber").set_value("1231231230")
-        # browser.element("#dateOfBirthInput").click()
-        # browser.element(".react-datepicker__month-select").s("July")
-        # browser.element(".react-datepicker__year-select").selectOption("2008")
-        # browser.element(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click()
         browser.element("#subjectsInput").send_keys("Maths")
         browser.element("#subjectsInput").press_enter()
         browser.element("#hobbiesWrapper").element(by.text("Sports")).click()
-        # browser.element("#uploadPicture").uploadFromClasspath("img/1.png")
         browser.element("#currentAddress").set_value("Some street 1")
         browser.element("#state").click()
         browser.element("#stateCity-wrapper").element(by.text("NCR")).click()
@@ -45,5 +57,9 @@ def test_successful():
 
     with allure.step("Check form results"):
         browser.element("#example-modal-sizes-title-lg").should(have.text("Thanks for submitting the form"))
-        # browser.element(".table-responsive").should(
-        #     have.texts(first_name, last_name, "alex@egorov.com", "Some street 1"))
+
+        from tests.utils import attach
+        attach.add_screenshot(browser)
+        attach.add_logs(browser)
+        attach.add_html(browser)
+        attach.add_video(browser)
